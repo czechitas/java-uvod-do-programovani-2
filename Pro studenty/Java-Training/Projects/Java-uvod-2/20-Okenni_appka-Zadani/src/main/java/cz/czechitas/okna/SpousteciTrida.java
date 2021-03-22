a3ef5cadc71631aba9b0768e6974575b6f29b4d6
Kamil Ševeček <kamil@sevecek.net>
2021-03-22 16:23:34 +0100

    Archive Okenni appka
    There is not enough time to go through this exercise so moving to the archive.

--- Pro studenty/Java-Training/Projects/Java-uvod-2/20-Okenni_appka-Zadani/src/main/java/cz/czechitas/okna/SpousteciTrida.java
+++
@@ -1,9 +0,0 @@
-package cz.czechitas.okna;
-
-public class SpousteciTrida {
-
-    public static void main(String[] args) {
-        System.out.println("Ahoj pozemstani, zavedte me ke svemu veliteli");
-    }
-
-}
